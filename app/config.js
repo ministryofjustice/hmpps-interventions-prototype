6e5316875cc1b3d7c82738ce5e3f7e0de4a9ba1c
Lawrence Forooghian <lawrence@dxw.com>
2020-08-25 14:22:23 +0100

    Set serviceName in prototype config

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'HMPPS - Find an intervention',
 
   // Default port that prototype runs on
   port: '3000',
