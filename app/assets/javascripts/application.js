d4b7521dc14cd8663c858585e46355f19eb25786
George Eaton <george@dxw.com>
2020-09-15 16:10:57 +0100

    Add save buttons to Referral screen
    These will be disabled unless they have text in them. This
    implementation is a bit hacky, but it should do for now. We may want to
    change the `onblur` handler to an `onkeyup` (which will be triggered any
    time a key is pressed) depending on testing, but this felt like we would
    be calling the function too many times. We could also poll this with a
    timeout if need be.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,9 +2,38 @@
 
 // Warn about using the kit in production
 if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
+  window.console.info("GOV.UK Prototype Kit - do not use for production");
+}
+
+function handleTextAreaSubmit(textAreaId, submitButtonId) {
+  const textArea = document.getElementById(textAreaId);
+  if (!textArea) {
+    return;
+  }
+
+  if (textArea.value === "") {
+    document.getElementById(submitButtonId).disabled = true;
+  } else {
+    document.getElementById(submitButtonId).disabled = false;
+  }
 }
 
 $(document).ready(function () {
-  window.GOVUKFrontend.initAll()
-})
+  window.GOVUKFrontend.initAll();
+  handleTextAreaSubmit(
+    "accommodation-desired-outcomes-info",
+    "accommodation-desired-outcomes-save"
+  );
+  handleTextAreaSubmit(
+    "accommodation-complexity-info",
+    "accommodation-complexity-save"
+  );
+  handleTextAreaSubmit(
+    "social-inclusion-complexity-info",
+    "social-inclusion-complexity-info-save"
+  );
+  handleTextAreaSubmit(
+    "social-inclusion-desired-outcomes-info",
+    "social-inclusion-desired-outcomes-save"
+  );
+});
