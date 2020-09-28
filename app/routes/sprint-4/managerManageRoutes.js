b480b0787ad64b35447fd1b5a8102b300547d38e
George Eaton <george@dxw.com>
2020-09-28 12:35:51 +0100

    Allow users to assign other interventions
    Users can now click on and assign different interventions and will be
    taken to those specific interventions (rather than defaulting to the
    first one as we were doing previously)
    
    This is a large commit, but modifies the shape of the session data
    defaults and manager static data so required changing a few sections of
    the prototype to access this data in the correct way.
    
    The data is still very duplicated at the moment, so I'd like to
    introduce more variety in future.
    
    Now that we can assign a few interventions, we can move "assigned"
    interventions into the respective tab on the referrals index page.

--- app/routes/sprint-4/managerManageRoutes.js
+++ app/routes/sprint-4/managerManageRoutes.js
@@ -5,7 +5,8 @@ const staticData = require("../../data/sprint-4/managerManageStaticData");
 
 router.use(function (req, res, next) {
   res.locals.serviceName = "Receive intervention referrals";
-  res.locals.serviceHref = "/sprint-4/book-and-manage/manage-a-referral/manager/dashboard";
+  res.locals.serviceHref =
+    "/sprint-4/book-and-manage/manage-a-referral/manager/dashboard";
   next();
 });
 
@@ -16,39 +17,66 @@ router.get("/dashboard", (req, res) => {
 
 router.get("/referrals", (req, res) => {
   res.render("sprint-4/book-and-manage/manage-a-referral/manager/referrals", {
-    probationPractitioners: staticData.probationPractitioners,
+    referrals: staticData.referrals,
   });
 });
 
-router.get("/referrals/:referralIndex/interventions/:interventionIndex", (req, res) => {
-  const serviceUser = staticData.serviceUsers[req.params.referralIndex];
-  const intervention = serviceUser.interventions[req.params.interventionIndex];
-  const caseworkers = staticData.caseworkers;
-  const reassign = req.query.reassign;
-  const currentCaseworker = { name: req.session.data['assigned-caseworker'] };
+router.get(
+  "/referrals/:referralIndex/interventions/:interventionIndex",
+  (req, res) => {
+    const referralIndex = req.params.referralIndex;
+    const interventionIndex = req.params.interventionIndex;
 
-  res.render("sprint-4/book-and-manage/manage-a-referral/manager/intervention", {
-    serviceUser,
-    intervention,
-    caseworkers,
-    reassign,
-    currentCaseworker,
-  });
-});
+    const referral = staticData.referrals[referralIndex];
+    const serviceUser = referral.serviceUser;
+    const intervention = referral.interventions[interventionIndex];
+    const caseworkers = staticData.caseworkers;
+    const reassign = req.query.reassign;
+    const currentCaseworker = { name: req.session.data["assigned-caseworker"] };
 
-router.get("/referrals/:referralIndex/interventions/:interventionIndex/confirm-details", (req, res) => {
-  const serviceUser = staticData.serviceUsers[req.params.referralIndex];
-  const intervention = serviceUser.interventions[req.params.interventionIndex];
+    res.render(
+      "sprint-4/book-and-manage/manage-a-referral/manager/intervention",
+      {
+        referralIndex,
+        interventionIndex,
+        serviceUser,
+        intervention,
+        caseworkers,
+        reassign,
+        currentCaseworker,
+      }
+    );
+  }
+);
 
-  res.render("sprint-4/book-and-manage/manage-a-referral/manager/confirm-details", {
-    serviceUser,
-    intervention,
-  });
-});
+router.get(
+  "/referrals/:referralIndex/interventions/:interventionIndex/confirm-details",
+  (req, res) => {
+    const referralIndex = req.params.referralIndex;
+    const interventionIndex = req.params.interventionIndex;
+
+    const referral = staticData.referrals[referralIndex];
+    const serviceUser = referral.serviceUser;
+    const intervention = referral.interventions[interventionIndex];
+    res.render(
+      "sprint-4/book-and-manage/manage-a-referral/manager/confirm-details",
+      {
+        referralIndex,
+        serviceUser,
+        intervention,
+      }
+    );
+  }
+);
 
 router.get("/referrals/:referralIndex/caseworker-confirmation", (req, res) => {
+  const referral = staticData.referrals[req.params.referralIndex];
+  const referralNumber = referral.referralNumber;
   res.render(
-    "sprint-4/book-and-manage/manage-a-referral/manager/caseworker-email-confirmation"
+    "sprint-4/book-and-manage/manage-a-referral/manager/caseworker-email-confirmation",
+    {
+      referralNumber,
+    }
   );
 });
 
