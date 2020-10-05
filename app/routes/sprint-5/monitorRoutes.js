4bb071b64dea914738290783c82fd8e34ce66b62
George Eaton <george@dxw.com>
2020-10-05 17:54:16 +0100

    Add query param filter to page
    And move "by-detail" view into own file

--- app/routes/sprint-5/monitorRoutes.js
+++ app/routes/sprint-5/monitorRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.get("/cases", (req, res) => {
   res.render("sprint-5/monitor/cases", {
     referrals: req.session.data.sprint5.referrals,
+    groupBy: req.query.group_by,
   });
 });
 
