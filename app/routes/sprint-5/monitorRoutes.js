fe97014ff0c3cacc51871dadc3ddc04f1ee96091
George Eaton <george@dxw.com>
2020-10-06 16:25:59 +0100

    Display active page in primary navigation

--- app/routes/sprint-5/monitorRoutes.js
+++ app/routes/sprint-5/monitorRoutes.js
@@ -5,6 +5,7 @@ router.get("/cases", (req, res) => {
   res.render("sprint-5/monitor/cases", {
     referrals: req.session.data.sprint5.referrals,
     groupBy: req.query.group_by,
+    currentPage: "cases",
   });
 });
 
@@ -12,6 +13,7 @@ router.get("/notifications", (req, res) => {
   res.render("sprint-5/monitor/notifications", {
     notifications: req.session.data.sprint5.notifications,
     sortBy: req.query.sort_by,
+    currentPage: "notifications",
   });
 });
 
