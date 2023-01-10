677215aecf7212b58aafaca35ddae5025d108281
Emma Hidalgo-Law <emma.hidalgo-law@digital.justice.gov.uk>
2023-01-10 12:24:15 +0000

    routes fix

--- app/routes.js
+++ app/routes.js
@@ -1,19 +1,10 @@
-const express = require('express');
-const router = express.Router();
-const app = express();
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
 
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
 
-
-// Add your routes here - above the module.exports line
-
-// Generic 'next page' rule
-router.post('*', function (req, res, next) {
-    console.log(req.body);
-    if (req.body['next-page']) {
-      res.redirect(req.body['next-page'])
-    } else {
-      next()
-    }
-  })
+// Add your routes here
 require('./views/v1/_routes.js')(router);
-module.exports = router
--- app/views/v1/_routes.js
+++ app/views/v1/_routes.js
@@ -2,7 +2,9 @@ module.exports = function(router) {
 
     var version = 'v1';
 
-    // make-a-referral
-    router.post('/make-a-referral', function(req, res) {
-        res.redirect('current-location');
+    // current-location
+    router.post('/current-location', function(req, res) {
+        res.redirect('release-date');
     })
+
+    };
