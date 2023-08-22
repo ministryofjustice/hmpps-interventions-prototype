02832b6694ae86da91f127da0043d2fa1438faa7
joanaaguia-moj <joana.aguia@digital.justice.gov.uk>
2023-08-22 17:56:53 +0100

    Added hint text under reschedule questions

--- app/views/v3/_routes.js
+++ app/views/v3/_routes.js
@@ -1,22 +1,12 @@
 module.exports = function (router) {
-var version = "v3"
-
-// router.get('/' + version + '/sp/did-they-attend', function (req, res) {
-//     res.render(version + '/sp/did-they-attend-2')
-//   });
-//  router.post('/' + version + '/sp/did-they-attend', function(request, response) {
-//     const didTheyAttend = req.session.data['did-they-attend']
-//            if ( didTheyAttend == 'yes') {
-//            response.redirect('/' + version + '/sp/session-happen');
-//         } else {
-//            response.redirect('/' + version + 'person-attend-session-does-not-happen');
-//         }
-//        });
-
+    var version = "v3"
+    
+    
+    
     router.get('/' + version + '/sp/did-they-attend', function(req, res){
         res.render(version + '/sp/did-they-attend')
     });
     router.post('/' + version + '/sp/did-they-attend', function(req,res){
-        res.redirect('/'+ version + '/sp/session-happen')
-    });
- }
+        res.redirect('/'+ version + '/sp/session-happen');
+    })
+     }
