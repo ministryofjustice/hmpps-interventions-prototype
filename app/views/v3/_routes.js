af4aa664da936fc94471dd3e796f5d0e791b4cd4
joanaaguia-moj <joana.aguia@digital.justice.gov.uk>
2023-08-23 13:49:47 +0100

    Changes to routes

--- app/views/v3/_routes.js
+++ app/views/v3/_routes.js
@@ -1,14 +1,27 @@
 module.exports = function (router) {
     var version = "v3" 
 
+    //if did-the-session-happen == yes and did-they-attend == yes 
+    //than show session-happen
+    //if did-the-session-happen == no and did-they-attend == yes
+    //than show you-told-us-that-the-person-did-attend
+    //if anything else == you-told-us-that-the-person-didnt-attend 
 
-     router.post('/' + version + '/sp/did-they-attend', function (req, res) {
-        const didTheyAttend = req.session.data['/sp/did-they-attend']
-    
-        if (didTheyAttend == 'yes'){
-          res.redirect('session-happen')
-        }else {
-          res.redirect('you-told-us-that-the-person-attended')
+      router.post ('/' + version + '/sp/did-the-session-happen', function (req, res) {
+       
+        var didTheSessionHappen = req.session.data['/'+ version + 'did-the-session-happen']
+        var didTheyAttend = req.session.data['/'+ version + 'did-they-attend']
+        
+        if (didTheSessionHappen == "yes" && didTheyAttend == "yes"){
+            response.redirect('session-happen')
+            
+        } else if (didTheSessionHappen == "no" && didTheyAttend == "yes"){
+            response.redirect('you-told-us-that-the-person-attended')
+            
+        } else {
+            res.redirect('sp/you-told-us-that-the-person-didnt-attend')
         }
       });
-    }
+ }
+
+        
