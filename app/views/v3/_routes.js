cc7a88a2035dd2ddc070d5eddec8117ac56a5bbb
joanaaguia-moj <joana.aguia@digital.justice.gov.uk>
2023-08-23 11:22:48 +0100

    Updated _route.js
    Updated _route.js to create 2 journeys

--- app/views/v3/_routes.js
+++ app/views/v3/_routes.js
@@ -1,12 +1,14 @@
 module.exports = function (router) {
-    var version = "v3"
+    var version = "v3" 
+
+
+     router.post('/' + version + '/sp/did-they-attend', function (req, res) {
+        const didTheyAttend = req.session.data['/sp/did-they-attend']
     
-    
-    
-    router.get('/' + version + '/sp/did-they-attend', function(req, res){
-        res.render(version + '/sp/did-they-attend')
-    });
-    router.post('/' + version + '/sp/did-they-attend', function(req,res){
-        res.redirect('/'+ version + '/sp/session-happen');
-    })
-     }
+        if (didTheyAttend == 'yes'){
+          res.redirect('session-happen')
+        }else {
+          res.redirect('you-told-us-that-the-person-attended')
+        }
+      });
+    }
