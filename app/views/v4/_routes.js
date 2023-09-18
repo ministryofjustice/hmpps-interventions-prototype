6cf02608d5bfdbe52e25d431dc0586f3bf6a76b9
joanaaguia-moj <joana.aguia@digital.justice.gov.uk>
2023-09-18 16:30:12 +0100

    Changes to designs

---
+++ app/views/v4/_routes.js
@@ -0,0 +1,30 @@
+module.exports = function (router) {
+    var version = "v4" 
+
+    //if did-the-session-happen == yes and did-they-attend == yes 
+    //than show session-happen
+    //if did-the-session-happen == no and did-they-attend == yes
+    //than show you-told-us-that-the-person-did-attend
+    //if anything else == you-told-us-that-the-person-didnt-attend 
+
+      router.post ('/' + version + '/sp/did-the-session-happen', function (request, response) {
+       
+        var didTheSessionHappen = request.session.data['did-the-session-happen']
+        var didTheyAttend = request.session.data['did-they-attend']
+        
+        if (didTheSessionHappen == "yes"){
+            response.redirect('session-happen')
+            
+        } else if (didTheSessionHappen == "no" && didTheyAttend == "yes"){
+            response.redirect('you-told-us-that-the-person-attended')
+            
+        } else {
+            response.redirect('you-told-us-that-the-person-didnt-attend')
+        }
+      });
+ }
+
+ 
+
+
+        
