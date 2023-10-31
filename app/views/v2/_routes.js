da40f1130e10eca64477c1d0c8a921836c3c4420
Emma Hidalgo Law <emma.hidalgo-law@digital.justice.gov.uk>
2023-10-31 16:03:57 +0000

    Pre release and unallocated prerelease iterations

--- app/views/v2/_routes.js
+++ app/views/v2/_routes.js
@@ -27,10 +27,33 @@ module.exports = function (router) {
         if (request.session.data['allocated-pp'] == 'yes') {
         response.redirect('referral-type-prison');
     } else if (request.session.data['allocated-pp'] == 'no') {
-        response.redirect('referral-type-prison');
+        response.redirect('');
     }
     })
 
+    router.post('/'+ version +'/pp/allocated-pp-community', function(request, response) {
+        if (request.session.data['allocated-pp-community'] == 'yes') {
+        response.redirect('referral-type-community');
+    } else if (request.session.data['allocated-pp-community'] == 'no') {
+        response.redirect('');
+    }
+    })
+
+    router.post('/'+ version +'/pp/allocated-pp-nocom-prerelease', function(request, response) {
+        if (request.session.data['allocated-pp-nocom-prerelease'] == 'yes') {
+        response.redirect('');
+    } else if (request.session.data['allocated-pp-nocom-prerelease'] == 'no') {
+        response.redirect('referral-type-nocom-prerelease');
+    }
+    })
+
+    router.post('/'+ version +'/pp/allocated-pp-nocom-prison', function(request, response) {
+        if (request.session.data['allocated-pp-nocom-prison'] == 'yes') {
+        response.redirect('');
+    } else if (request.session.data['allocated-pp-nocom-prison'] == 'no') {
+        response.redirect('referral-type-nocom-prison');
+    }
+    })
 
 
 
