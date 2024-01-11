749855db9320dbdb3861136223424932aa5d8c04
joanaaguia-moj <joana.aguia@digital.justice.gov.uk>
2024-01-11 15:34:38 +0000

    Branching of accept a referral journey

--- app/views/v4/_routes.js
+++ app/views/v4/_routes.js
@@ -7,24 +7,28 @@ module.exports = function (router) {
     //than show you-told-us-that-the-person-did-attend
     //if anything else == you-told-us-that-the-person-didnt-attend 
 
-      router.post ('/' + version + '/sp/did-the-session-happen', function (request, response) {
-       
-        var didTheSessionHappen = request.session.data['did-the-session-happen']
-        var didTheyAttend = request.session.data['did-they-attend']
-        
-        if (didTheSessionHappen == "yes"){
-            response.redirect('session-happen')
-            
-        } else if (didTheSessionHappen == "no" && didTheyAttend == "yes"){
-            response.redirect('you-told-us-that-the-person-attended')
+    router.get('/' + version + '/pp/cancellation-request', function (req, res) {
+        res.render(version + '/pp/cancellation-request')
+      });
 
-        } else if (didTheSessionHappen == "no" && didTheyAttend == "no"){
-            response.redirect('you-told-us-that-the-person-didnt-attend')
+      router.post ('/' + version + '/pp/cancellation-request', function (request, response) {
+       
+        const doYouWantToCancel = request.session.data['do-you-want-to-cancel']
+        
+        
+        if (doYouWantToCancel == 'yes'){
             
+           response.redirect('case-progress-assigned-alex-cancellation-request-sent')
+        } else if (doYouWantToCancel == "no"){
+            response.redirect('case-progress-assigned-alex-no-cancellation')
+
         } else {
-            response.redirect('dont-know-if-person-attended')
+            response.redirect('what-parts-of-referral-would-you-like-to-change')
         }
       });
+
+
+
  }
 
  
