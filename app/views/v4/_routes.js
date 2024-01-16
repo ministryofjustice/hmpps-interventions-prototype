8c4e8680b621c88a3a08a85a575bfcbefa4fa09f
joanaaguia-moj <joana.aguia@digital.justice.gov.uk>
2024-01-16 16:47:56 +0000

    Fixed javascript on previous version

--- app/views/v4/_routes.js
+++ app/views/v4/_routes.js
@@ -27,9 +27,36 @@ module.exports = function (router) {
         }
       });
 
+      router.post ('/' + version + '/sp/did-the-session-happen', function (request, response) {
+       
+        var didTheSessionHappen = request.session.data['did-the-session-happen']
+        var didTheyAttend = request.session.data['did-they-attend']
+        
+        if (didTheSessionHappen == "yes"){
+            response.redirect('session-happen')
+            
+        } else if (didTheSessionHappen == "no" && didTheyAttend == "yes"){
+            response.redirect('you-told-us-that-the-person-attended')
+      
+        } else if (didTheSessionHappen == "no" && didTheyAttend == "no"){
+            response.redirect('you-told-us-that-the-person-didnt-attend')
+            
+        } else {
+            response.redirect('dont-know-if-person-attended')
+        }
+      });
+
+
+
 
 
  }
+
+
+ 
+
+
+
 
  
 
